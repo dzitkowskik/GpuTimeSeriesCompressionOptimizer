986c2bc086bf9b0a6b7f73fa570458685f28ab2c
Karol Dzitkowski <k.dzitkowski@gmail.com>
2015-04-23 14:10:08 +0200

    fix helper comparison

---
+++ src/helpers/helper_comparison.cu
@@ -0,0 +1,36 @@
+#include "helper_comparison.cuh"
+
+// ulp = units in the last place; maxulps = maximum number of
+// representable floating point numbers by which x and y may differ.
+__host__ __device__ bool _floatsEqual(float a, float b, int maxulps)
+{
+	// convert to integer.
+	int aint = *(int*)&a;
+	int bint = *(int*)&b;
+	// make lexicographically ordered as a twos-complement int
+	if (aint < 0) aint = 0x80000000 - aint;
+	if (bint < 0) bint = 0x80000000 - bint;
+	// compare.
+	return abs(aint - bint) <= maxulps;
+}
+
+__global__ void _compareFloatsKernel(float* a, float* b, int size, bool* out)
+{
+	unsigned int iElement = blockDim.x * blockIdx.x + threadIdx.x;
+	if (iElement >= size) return;
+	out[iElement] = !_floatsEqual(a[iElement], b[iElement], 5);
+}
+
+__host__ bool CompareDeviceFloatArrays(float* a, float* b, int size)
+{
+    bool* out;
+    CUDA_CALL(cudaMalloc((void**)&out, size*sizeof(float)));
+    int blocks = (size + COMP_THREADS_PER_BLOCK - 1) / COMP_THREADS_PER_BLOCK;
+    _compareFloatsKernel<<<blocks, COMP_THREADS_PER_BLOCK>>>(a, b, size, out);
+    cudaDeviceSynchronize();
+    thrust::device_vector<bool> out_vector(out, out+size);
+    thrust::inclusive_scan(out_vector.begin(), out_vector.end(), out_vector.begin());
+    int result = out_vector.back();
+    CUDA_CALL(cudaFree(out));
+    return result == 0;
+}
