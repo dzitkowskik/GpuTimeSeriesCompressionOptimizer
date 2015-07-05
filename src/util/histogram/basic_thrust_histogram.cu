96bd11f8b6537f6a3b6bd62bb8a0daff56d53e2c
Karol Dzitkowski <k.dzitkowski@gmail.com>
2015-07-05 20:56:11 +0200

    Simple histogram using thrust implemented + one unit test

--- src/util/histogram/basic_thrust_histogram.cu
+++ src/util/histogram/basic_thrust_histogram.cu
@@ -1,10 +1,33 @@
 #include "basic_thrust_histogram.cuh"
+#include <thrust/device_vector.h>
+#include <thrust/reduce.h>
 
 namespace ddj {
 
 SharedCudaPtrPair<int, int> BasicThrustHistogram::IntegerHistogram(SharedCudaPtr<int> data)
 {
-    return SharedCudaPtrPair<int, int>(SharedCudaPtr<int>(), SharedCudaPtr<int>());
+    thrust::device_ptr<int> data_ptr(data->get());
+    thrust::device_vector<int> output(data->size());
+    thrust::device_vector<int> lengths(data->size());
+
+    auto result = thrust::reduce_by_key(
+        data_ptr,
+        data_ptr+data->size(),
+        thrust::constant_iterator<int>(1),
+        output.begin(),
+        lengths.begin());
+
+    int len = result.first - output.begin();
+
+    auto keys = CudaPtr<int>::make_shared(len);
+    auto counts = CudaPtr<int>::make_shared(len);
+    thrust::device_ptr<int> keys_ptr(keys->get());
+    thrust::device_ptr<int> counts_ptr(counts->get());
+
+    thrust::copy(output.begin(), output.begin()+len, keys_ptr);
+    thrust::copy(lengths.begin(), lengths.begin()+len, counts_ptr);
+
+    return SharedCudaPtrPair<int, int>(keys, counts);
 }
 
 } /* namespace ddj */
